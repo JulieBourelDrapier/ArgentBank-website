0b0cc398c7920781800b32d90bef39a8808ef989
JulieBourelDrapier <juliebourel@hotmail.com>
2023-09-25 16:54:57 +0200

    create nav.jsx with styled-components based on main.css

---
+++ front/src/components/nav.jsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+const MainNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px 20px;
+`;
+
+const MainNavLogo = styled.a`
+  font-weight: bold;
+  color: #2c3e50;
+`;
+
+const MainNavLogoImg = styled.img`
+  max-width: 100%;
+  width: 200px;
+`;
+
+const SrOnly = styled.h1`
+  border: 0 !important;
+  clip: rect(1px, 1px, 1px, 1px) !important; /* 1 */
+  -webkit-clip-path: inset(50%) !important;
+  clip-path: inset(50%) !important; /* 2 */
+  height: 1px !important;
+  margin: -1px !important;
+  overflow: hidden !important;
+  padding: 0 !important;
+  position: absolute !important;
+  width: 1px !important;
+  white-space: nowrap !important; /* 3 */
+`;
+
+const MainNavItem = styled.a`
+  text-decoration: none;
+  margin-right: 0.5rem;
+`;
+
+const MainNavIcon = styled.i`
+`;
+
+function Nav () {
+  return (
+    <MainNav>
+      <MainNavLogo href="./index.html">
+        <MainNavLogoImg
+          src="./img/argentBankLogo.png"
+          alt="Argent Bank Logo"
+        />
+        <SrOnly>Argent Bank</SrOnly>
+      </MainNavLogo>
+      <div>
+        <MainNavItem href="./sign-in.html">
+          <MainNavIcon class="fa fa-user-circle"></MainNavIcon>
+          Sign In
+        </MainNavItem>
+      </div>
+</MainNav>
+  );
+}
+
+export default Nav;
