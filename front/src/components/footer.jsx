e4284ae0fb0183b3d4b6b50b27360aa22a2dcf1d
JulieBourelDrapier <juliebourel@hotmail.com>
2023-09-25 16:35:57 +0200

    creation of footer.jsx (add style from main.css) with styled-components

---
+++ front/src/components/footer.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+  border-top: 2px solid #ccc;
+  padding: 2rem 0 1.5rem;
+`;
+
+const FooterText = styled.p`
+  margin: 0;
+  padding: 0;
+`;
+
+function Footer () {
+  return (
+    <Footer>
+      <FooterText>
+        Copyright 2020 Argent Bank
+      </FooterText>
+    </Footer>
+  )
+}
+
+export default Footer;
