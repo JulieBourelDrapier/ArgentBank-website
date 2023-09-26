41a2199662f113acc541f8e7c30f261e4bb1e57b
JulieBourelDrapier <juliebourel@hotmail.com>
2023-09-26 22:28:33 +0200

    Add signIn.jsx page

---
+++ front/src/pages/user.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './components/footer';	
+import Nav from './components/nav';
+import './style.css';
+
+function User() {
+  return (
+    <div>
+      <Nav />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default User;
