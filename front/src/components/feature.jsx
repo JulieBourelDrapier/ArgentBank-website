0b0b4f9f834ad3256763e3bb541c20d0fb6156ca
JulieBourelDrapier <juliebourel@hotmail.com>
2023-09-27 13:53:49 +0200

    props to Feature

--- front/src/components/feature.jsx
+++ front/src/components/feature.jsx
@@ -1,42 +1,13 @@
 import React from "react";
 import '../style.css';
 
-function Feature() {
+function Feature({img, title, desc}) {
   return (
-    <section className="features">
-          <h2 className="sr-only">Features</h2>
-          <div className="feature-item">
-            <img src="./img/icon-chat.webp" alt="Chat Icon" className="feature-icon" />
-            <h3 className="feature-item-title">You are our #1 priority</h3>
-            <p>
-              Need to talk to a representative? You can get in touch through our
-              24/7 chat or through a phone call in less than 5 minutes.
-            </p>
-          </div>
-          <div className="feature-item">
-            <img
-              src="./img/icon-money.webp"
-              alt="Chat Icon"
-              className="feature-icon"
-            />
-            <h3 className="feature-item-title">More savings means higher rates</h3>
-            <p>
-              The more you save with us, the higher your interest rate will be!
-            </p>
-          </div>
-          <div className="feature-item">
-            <img
-              src="./img/icon-security.webp"
-              alt="Chat Icon"
-              className="feature-icon"
-            />
-            <h3 className="feature-item-title">Security you can trust</h3>
-            <p>
-              We use top of the line encryption to make sure your data and money
-              is always safe.
-            </p>
-          </div>
-        </section>
+    <div className="feature-item">
+      <img src={img.src} alt={img.alt} className="feature-icon" />
+      <h3 className="feature-item-title">{title}</h3>
+      <p>{desc}</p>
+    </div>
   );
 }
 
