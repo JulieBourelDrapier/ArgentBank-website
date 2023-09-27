8503122a61fe1aa8553f15e63ed9fabda7d7a807
JulieBourelDrapier <juliebourel@hotmail.com>
2023-09-27 22:00:52 +0200

    fix component

--- front/src/components/account.jsx
+++ front/src/components/account.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import '../style.css';
 
-function Account() {
+function Account({title, amount, desc}) {
   return (
     <section className="account">
       <div className="account-content-wrapper">
-        <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-        <p className="account-amount">$2,082.79</p>
-        <p className="account-amount-description">Available Balance</p>
+        <h3 className="account-title">{title}</h3>
+        <p className="account-amount">{amount}</p>
+        <p className="account-amount-description">{desc}</p>
       </div>
       <div className="account-content-wrapper cta">
         <button className="transaction-button">View transactions</button>
--- front/src/pages/user.jsx
+++ front/src/pages/user.jsx
@@ -11,9 +11,9 @@ function User() {
           <button className="edit-button">Edit Name</button>
         </div>
         <h2 className="sr-only">Accounts</h2>
-       <Account />
-       <Account />
-       <Account />
+        <Account title="Argent Bank Checking (x8349)" amount="$2,082.79" desc="Available Balance" />
+        <Account title="Argent Bank Savings (x6712)" amount="$10,928.42" desc="Available Balance" />
+        <Account title="Argent Bank Credit Card (x8349)" amount="$184.30" desc="Current Balance" />
       </main>
     </body>
   );
