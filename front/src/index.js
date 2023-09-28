62b5513b7144253207e52a21dcc5b811e31faecb
JulieBourelDrapier <juliebourel@hotmail.com>
2023-09-28 22:15:12 +0200

    redux param

--- front/src/index.js
+++ front/src/index.js
@@ -1,24 +1,40 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+// REDUX
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from './reducers/index';
+
 import Nav from './components/nav';
 import Home from './pages/home';
 import SignIn from './pages/signIn';
 import User from './pages/user';
 import Footer from './components/footer';
 
+// METTRE DEVTOOLS FALSE AVANT MISE EN PROD
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/user" element={<User />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/user" element={<User />} />
+        </Routes>
+        <Footer />
+      </Router>
+      </Provider> 
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+
